f08e21e0a9cdc5078289dd50202186ad2c25e17f
mraf19 <raflimr1902@gmail.com>
2023-04-05 09:58:43 +0700

    Adding Favprite Page

---
+++ pages/favorite.tsx
@@ -0,0 +1,21 @@
+import Card from "@/components/Card";
+
+export default function Favorite() {
+  return (
+    <>
+      <div className="container mx-auto mt-20">
+        <h1 className="text-center text-5xl font-bold text-slate-900">
+          Favorite Movie
+        </h1>
+        <div className="flex flex-wrap items-center justify-center gap-10 mt-10 mx-auto">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    </>
+  );
+}
