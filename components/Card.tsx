11209f35982425daf71555e81966e77beb1e124c
mraf19 <raflimr1902@gmail.com>
2023-04-07 07:41:13 +0700

    edit navbar

--- components/Card.tsx
+++ components/Card.tsx
@@ -16,8 +16,7 @@ export default function Card({ movie }: MovieType) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [hoverIndex, setHoverIndex] = useState(0);
   const [rating, setRating] = useState(0);
-  const { data: session } = useSession() 
-  console.log(movie.imageUrl)
+  const { data: session } = useSession();
   return (
     <div
       id="Card"
@@ -37,73 +36,72 @@ export default function Card({ movie }: MovieType) {
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
           {movie.description}
         </p>
-        {
-          session && <div>
-          <a
-            onClick={() => setIsFavorite(!isFavorite)}
-            className={`inline-flex items-center px-3 py-2 mr-3 text-sm font-medium text-center text-slate-50 ${
-              isFavorite
-                ? "bg-orange-500 hover:bg-orange-600"
-                : "bg-yellow-300 hover:bg-yellow-400"
-            } rounded-lg focus:ring-4 focus:outline-none focus:ring-gray-300 shadow-md dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
-          >
-            {isFavorite ? (
-              <MinusCircleIcon className="w-[20px] h-[20px] mr-2" />
-            ) : (
-              <PlusCircleIcon className="w-[20px] h-[20px] mr-2" />
-            )}
-            {isFavorite ? "Remove from Favorite" : "Add to Favorite"}
-          </a>
-          <a
-            onClick={() => setIsModalOpen(!isModalOpen)}
-            className={`inline-flex items-center px-3 py-2 text-sm font-medium text-center text-slate-50 bg-green-500 hover:bg-green-600 rounded-lg focus:ring-4 focus:outline-none focus:ring-gray-300 shadow-md dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
-          >
-            <ChatBubbleBottomCenterTextIcon className="w-[20px] h-[20px] mr-1" />
-            Rate
-          </a>
-          <ReactModal
-            className="bg-slate-100 font-semibold text-xl text-slate-700 w-[50%] h-[50%] mx-auto my-[20px] border-slate-500 flex flex-col justify-center items-center"
-            isOpen={isModalOpen}
-          >
-            <h2>Rate this film</h2>
-            <div className="my-4 text-center">
-              <ul>
-                {[1, 2, 3, 4, 5].map((index: number) => (
-                  <li
-                    onMouseEnter={() => setHoverIndex(index)}
-                    onMouseLeave={() => setHoverIndex(0)}
-                    onClick={() => setRating(index)}
-                    className="inline-flex"
-                    key={`star-${index}`}
-                  >
-                    <Star yellow={index <= hoverIndex || index <= rating} />
-                  </li>
-                ))}
-              </ul>
-              <span className="text-center text-3xl">
-                {rating === 1
-                  ? "Awful!!"
-                  : rating === 2
-                  ? "Bad!"
-                  : rating === 3
-                  ? "Good!"
-                  : rating === 4
-                  ? "Awesome!!"
-                  : rating === 5
-                  ? "Perfect!!!"
-                  : ""}
-              </span>
-            </div>
-            <button
-              onClick={() => setIsModalOpen(false)}
-              className={`inline-flex items-center px-3 py-2 my-4 text-sm font-medium text-center text-slate-50 bg-gray-700 hover:bg-gray-800 rounded-lg focus:ring-4 focus:outline-none focus:ring-gray-300 shadow-md dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
+        {session && (
+          <div>
+            <a
+              onClick={() => setIsFavorite(!isFavorite)}
+              className={`inline-flex items-center px-3 py-2 mr-3 text-sm font-medium text-center text-slate-50 ${
+                isFavorite
+                  ? "bg-orange-500 hover:bg-orange-600"
+                  : "bg-yellow-300 hover:bg-yellow-400"
+              } rounded-lg focus:ring-4 focus:outline-none focus:ring-gray-300 shadow-md dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
             >
-              Close
-            </button>
-          </ReactModal>
-        </div>
-        }
-        
+              {isFavorite ? (
+                <MinusCircleIcon className="w-[20px] h-[20px] mr-2" />
+              ) : (
+                <PlusCircleIcon className="w-[20px] h-[20px] mr-2" />
+              )}
+              {isFavorite ? "Remove from Favorite" : "Add to Favorite"}
+            </a>
+            <a
+              onClick={() => setIsModalOpen(!isModalOpen)}
+              className={`inline-flex items-center px-3 py-2 text-sm font-medium text-center text-slate-50 bg-green-500 hover:bg-green-600 rounded-lg focus:ring-4 focus:outline-none focus:ring-gray-300 shadow-md dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
+            >
+              <ChatBubbleBottomCenterTextIcon className="w-[20px] h-[20px] mr-1" />
+              Rate
+            </a>
+            <ReactModal
+              className="bg-slate-100 font-semibold text-xl text-slate-700 w-[50%] h-[50%] mx-auto my-[20px] border-slate-500 flex flex-col justify-center items-center"
+              isOpen={isModalOpen}
+            >
+              <h2>Rate this film</h2>
+              <div className="my-4 text-center">
+                <ul>
+                  {[1, 2, 3, 4, 5].map((index: number) => (
+                    <li
+                      onMouseEnter={() => setHoverIndex(index)}
+                      onMouseLeave={() => setHoverIndex(0)}
+                      onClick={() => setRating(index)}
+                      className="inline-flex"
+                      key={`star-${index}`}
+                    >
+                      <Star yellow={index <= hoverIndex || index <= rating} />
+                    </li>
+                  ))}
+                </ul>
+                <span className="text-center text-3xl">
+                  {rating === 1
+                    ? "Awful!!"
+                    : rating === 2
+                    ? "Bad!"
+                    : rating === 3
+                    ? "Good!"
+                    : rating === 4
+                    ? "Awesome!!"
+                    : rating === 5
+                    ? "Perfect!!!"
+                    : ""}
+                </span>
+              </div>
+              <button
+                onClick={() => setIsModalOpen(false)}
+                className={`inline-flex items-center px-3 py-2 my-4 text-sm font-medium text-center text-slate-50 bg-gray-700 hover:bg-gray-800 rounded-lg focus:ring-4 focus:outline-none focus:ring-gray-300 shadow-md dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
+              >
+                Close
+              </button>
+            </ReactModal>
+          </div>
+        )}
       </div>
     </div>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,34 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
-import { useSession } from "next-auth/react"
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { Bars3Icon } from "@heroicons/react/24/solid";
 
 export default function Navbar() {
   const router = useRouter();
-  const { data: session, status } = useSession()
+  const [isOpen, setIsOpen] = useState(false);
+  const { data: session } = useSession();
   return (
-    <nav className="fixed top-0 w-full border-b-[1px] border-gray-700 bg-white  dark:bg-gray-900">
+    <nav
+      className={`md:border-b-[1px] fixed top-0 w-full border-gray-700 bg-white  dark:bg-gray-900`}
+    >
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center justify-start">
           <span className="self-center text-5xl font-semibold whitespace-nowrap dark:text-white font-mono">
             Movie App
           </span>
         </a>
-        <div className="hidden w-full md:block md:w-auto">
+        <div onClick={() => setIsOpen(!isOpen)} className="md:hidden">
+          <Bars3Icon className="w-[50px] h-[50px]" />
+        </div>
+        <div className={`${isOpen ? "" : "hidden"} w-full md:block md:w-auto`}>
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
                 href="/"
                 className={`block py-2 pl-3 pr-4 ${
                   router.route === "/" ? "text-blue-500" : "text-gray-900"
-                } md:active:text-blue-700 md:hover:text-blue-700 bg-blue-700 rounded md:bg-transparent md:p-0 dark:text-white md:dark:text-blue-500`}
+                } md:active:text-blue-700 md:hover:text-blue-700 rounded md:bg-transparent md:p-0 dark:text-white md:dark:text-blue-500`}
               >
                 Home
               </Link>
@@ -59,33 +67,25 @@ export default function Navbar() {
                 Favorite
               </Link>
             </li>
-            {
-              session ? (<li>
+            {session ? (
+              <li>
                 <Link
                   href="/api/auth/signout"
-                  className={`block py-2 pl-3 pr-4 ${
-                    router.route === "/favorite"
-                      ? "text-blue-500"
-                      : "text-gray-900"
-                  } rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}
+                  className={`block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}
                 >
                   Sign Out
                 </Link>
-              </li>) : (<li>
-              <Link
-                href="/api/auth/signin"
-                className={`block py-2 pl-3 pr-4 ${
-                  router.route === "/favorite"
-                    ? "text-blue-500"
-                    : "text-gray-900"
-                } rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}
-              >
-                Sign In
-              </Link>
-            </li>)
-            }
-            
-            
+              </li>
+            ) : (
+              <li>
+                <Link
+                  href="/api/auth/signin"
+                  className={`block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}
+                >
+                  Sign In
+                </Link>
+              </li>
+            )}
           </ul>
         </div>
       </div>
